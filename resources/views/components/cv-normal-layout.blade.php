6f242219a63404f81cdb37b67a4e65028331c1b2
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2024-10-19 22:21:27 +0700

    fix user side error
    Mixed Content: The page at 'https://web.fly.dev/ was loaded over HTTPS, but requested an insecure stylesheet 'http://web.fly.dev/build/assets/file.css'. This request has been blocked; the content must be served over HTTPS.

--- resources/views/components/cv-normal-layout.blade.php
+++ resources/views/components/cv-normal-layout.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    {{-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> --}}
     {{-- {{ HTML::style('css/cv.css') }} --}}
     {{-- {{ URL::asset('css/css.css') }} --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
